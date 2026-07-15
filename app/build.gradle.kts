047d6eba7b17f06a0249eda3ffc7d03cb05c31b5
ciddwd <2117971372@qq.com>
2026-07-15 08:31:24 -0700

    chore: 准备 0.3.9 发布
    - 将 versionName 更新为 0.3.9
    
    - 将 versionCode 从 11 递增至 12

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "com.gameocr.app"
         minSdk = 26
         targetSdk = 35
-        versionCode = 11
-        versionName = "0.3.8"
+        versionCode = 12
+        versionName = "0.3.9"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables.useSupportLibrary = true
